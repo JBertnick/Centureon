a3c03be98c099166a265869d082b0b7e94e3cdf5
dlaarschot <dlaarschot@yahoo.co.uk>
2020-02-11 13:48:02 +0000

    Static Files

---
+++ static/js/dashboard.js
@@ -0,0 +1,52 @@
+/* globals Chart:false, feather:false */
+
+(function() {
+    'use strict'
+
+
+    // Graphs
+    var ctx = document.getElementById('myChart')
+        // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday'
+            ],
+            datasets: [{
+                data: [
+                    5,
+                    10,
+                    18,
+                    20,
+                    22,
+                    30,
+                    32
+                ],
+                lineTension: 0,
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                borderWidth: 4,
+                pointBackgroundColor: '#007bff'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            }
+        }
+    })
+}())
